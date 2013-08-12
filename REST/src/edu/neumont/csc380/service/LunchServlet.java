10961a7d945d0e250d6a6c428ba554df2be81b82
wes-cutting <wesleycutting@gmail.com>
2013-08-12 08:25:20 -0600

    Basic REST implemented with Restaurant List

--- REST/src/edu/neumont/csc380/service/LunchServlet.java
+++ REST/src/edu/neumont/csc380/service/LunchServlet.java
@@ -1,9 +1,17 @@
 package edu.neumont.csc380.service;
 
-import javax.ws.rs.GET;
+import javax.jws.WebMethod;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,11 +21,39 @@ import javax.ws.rs.core.MediaType;
  * To change this template use File | Settings | File Templates.
  */
 @WebServlet(name = "LunchServlet", urlPatterns = "/lunch")
-public class LunchServlet {
+public class LunchServlet extends HttpServlet{
 
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public String returnTitle(){
-        return "<p>Lunch is served</p>";
+    private static final List<Restaurant> restaurants;
+
+    static{
+        restaurants = new ArrayList<Restaurant>();
+        Restaurant r1 = new Restaurant();
+        r1.setName("Chick-fil-a");
+        r1.addMenuItem("Chicken Sammich");
+        r1.addMenuItem("Waffle Fries");
+        Restaurant r2 = new Restaurant();
+        r2.setName("Red Hot");
+        r2.addMenuItem("5th Ave.");
+        r2.addMenuItem("Chili Dog");
+        r2.addMenuItem("Kraut Dog");
+        Restaurant r3 = new Restaurant();
+        r3.setName("iHop");
+        r3.addMenuItem("Pancakes");
+        r3.addMenuItem("Bacon & Eggs");
+        r3.addMenuItem("Coffee");
+        r3.addMenuItem("Omelet");
+        restaurants.add(r1);
+        restaurants.add(r2);
+        restaurants.add(r3);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+
+        for(Restaurant r: restaurants){
+            out.println(r.toString());
+        }
+
     }
 }
